2b52b1740378cfaf702e5e0f94797070e46a8758
Michael Badaracca - Elastic Elephant <michael.badaracca@elasticElephant.com>
2017-05-16 23:33:09 -0600

    Stubbed out general files with some comments about what should be in the files

---
+++ ChatClient/index.js
@@ -0,0 +1,22 @@
+//import libraries
+
+var expresss = require("express");
+
+var app = express();
+
+//initialize express
+//use body parser
+
+//set up route to get general files
+    //will need this to serve up an index.html
+    //will need this to serve up a login page
+
+//set up authentication check
+
+//set up route to send chats to common thread
+
+//set up 404 route
+
+//set up 500 route
+
+//setup port
