092fd8bb65945b7c274f1676e591a70fddcd5263
Michael Badaracca - Elastic Elephant <michael.badaracca@elasticElephant.com>
2017-05-17 00:04:14 -0600

    Created some basic middleware layers

--- ChatClient/index.js
+++ ChatClient/index.js
@@ -1,8 +1,11 @@
-//import libraries
+//import modules
+var express = require("express");
 
-var expresss = require("express");
-
+//Global Variables
+var PORT = 8000;
 var app = express();
+
+app.use(express.static("public"));
 
 //initialize express
 //use body parser
@@ -17,6 +20,22 @@ var app = express();
 
 //set up 404 route
 
+app.use(function (req, res, next) {
+    console.log(`404 page not found for ${req}`);
+    res.status = 404;
+    res.send("404 Error - resource not found");
+})
+
 //set up 500 route
 
+app.use(function (err, req, res, next) {
+    console.log(err);
+    res.status = 500;
+    res.send("500 Internal Server Error ");
+});
+
 //setup port
+
+app.listen(PORT, function () {
+    console.log(`Server running on port ${PORT}`);
+})
